0655f10e172c852d120ef9828e59b5faef0535de
Xudong Huang <huangxu008@hotmail.com>
2018-03-01 15:22:12 +0800

    :gift: support pipeline server

--- src/http_server.rs
+++ src/http_server.rs
@@ -1,9 +1,10 @@
 //! http server implementation on top of `MAY`
 
-use std::error::Error;
-use std::io::{self, Read, Write};
-use std::net::ToSocketAddrs;
 use std::sync::Arc;
+use std::error::Error;
+use std::net::ToSocketAddrs;
+use std::collections::VecDeque;
+use std::io::{self, Read, Write};
 
 use may::coroutine;
 use may::net::TcpListener;
@@ -17,11 +18,6 @@ use response::{self, Response};
 pub trait HttpService {
     fn call(&self, _request: Request) -> io::Result<Response>;
 }
-
-/// this is the generic type http server
-/// with a type parameter that impl `HttpService` trait
-///
-pub struct HttpServer<T>(pub T);
 
 macro_rules! t {
     ($e: expr) => (match $e {
@@ -56,6 +52,11 @@ fn internal_error_rsp(e: io::Error) -> Response {
     err_rsp
 }
 
+/// this is the generic type http server
+/// with a type parameter that impl `HttpService` trait
+///
+pub struct HttpServer<T>(pub T);
+
 impl<T: HttpService + Send + Sync + 'static> HttpServer<T> {
     /// Spawns the http service, binding to the given address
     /// return a coroutine that you can cancel it when need to stop the service
@@ -69,34 +70,38 @@ impl<T: HttpService + Send + Sync + 'static> HttpServer<T> {
                     let mut stream = t_c!(stream);
                     let server = server.clone();
                     go!(move || {
-                        let mut buf = BytesMut::with_capacity(512);
-                        let mut rsp = BytesMut::with_capacity(512);
+                        let mut buf = BytesMut::with_capacity(4096);
+                        let mut reqs = VecDeque::with_capacity(32);
+                        let mut rsps = BytesMut::with_capacity(4096);
                         loop {
-                            match t!(request::decode(&mut buf)) {
-                                None => {
-                                    // need more data
-                                    if buf.remaining_mut() < 256 {
-                                        buf.reserve(512);
-                                    }
-                                    let n = {
-                                        let read_buf = unsafe { buf.bytes_mut() };
-                                        t!(stream.read(read_buf))
-                                    };
-                                    if n == 0 {
-                                        //connection was closed
-                                        return;
-                                    }
-                                    unsafe { buf.advance_mut(n) };
-                                }
-                                Some(req) => {
-                                    let ret = server.0.call(req).unwrap_or_else(internal_error_rsp);
-                                    response::encode(ret, &mut rsp);
-
-                                    // send the result back to client
-                                    t!(stream.write_all(rsp.as_ref()));
-                                    rsp.clear();
-                                }
+                            // read the socket for reqs
+                            if buf.remaining_mut() < 1024 {
+                                buf.reserve(4096);
                             }
+                            let n = {
+                                let read_buf = unsafe { buf.bytes_mut() };
+                                t!(stream.read(read_buf))
+                            };
+                            //connection was closed
+                            if n == 0 {
+                                return;
+                            }
+                            unsafe { buf.advance_mut(n) };
+
+                            // prepare the reqs
+                            while let Some(req) = t!(request::decode(&mut buf)) {
+                                reqs.push_back(req);
+                            }
+
+                            // process the reqs
+                            while let Some(req) = reqs.pop_front() {
+                                let ret = server.0.call(req).unwrap_or_else(internal_error_rsp);
+                                response::encode(ret, &mut rsps);
+                            }
+
+                            // send the result back to client
+                            t!(stream.write_all(rsps.as_ref()));
+                            rsps.clear();
                         }
                     });
                 }
