62f68da5311e11000076dea6fb55e3c8ea899a45
Xudong Huang <huangxu008@hotmail.com>
2020-02-15 16:49:36 +0800

    :pencil: use single io wait for unix

--- src/http_server.rs
+++ src/http_server.rs
@@ -5,7 +5,11 @@ use std::net::ToSocketAddrs;
 
 use crate::request::{self, Request};
 use crate::response::{self, Response};
+#[cfg(unix)]
+use bytes::Buf;
 use bytes::{BufMut, BytesMut};
+#[cfg(unix)]
+use may::io::WaitIo;
 use may::net::{TcpListener, TcpStream};
 use may::{coroutine, go};
 
@@ -87,6 +91,96 @@ fn internal_error_rsp(e: io::Error, buf: &mut BytesMut) -> Response {
 ///
 pub struct HttpServer<T>(pub T);
 
+#[cfg(unix)]
+fn each_connection_loop<T: HttpService>(mut stream: TcpStream, mut service: T) {
+    let mut req_buf = BytesMut::with_capacity(4096 * 8);
+    let mut rsp_buf = BytesMut::with_capacity(4096 * 32);
+    let mut body_buf = BytesMut::with_capacity(4096 * 8);
+    stream.set_nonblocking(true).unwrap();
+    loop {
+        stream.reset_io();
+        loop {
+            // read the socket for reqs
+            if req_buf.capacity() - req_buf.len() < 512 {
+                req_buf.reserve(4096 * 8);
+            }
+
+            let read_buf = unsafe { &mut *(req_buf.bytes_mut() as *mut _ as *mut [u8]) };
+            match stream.read(read_buf) {
+                Ok(n) => {
+                    if n == 0 {
+                        //connection was closed
+                        return;
+                    } else {
+                        unsafe { req_buf.advance_mut(n) };
+                    }
+                }
+                Err(err) => {
+                    if err.kind() == io::ErrorKind::WouldBlock {
+                        break;
+                    } else if err.kind() == io::ErrorKind::ConnectionReset
+                        || err.kind() == io::ErrorKind::UnexpectedEof
+                    {
+                        // info!("http server read req: connection closed");
+                        return;
+                    }
+                    error!("call = {:?}\nerr = {:?}", stringify!($e), err);
+                    return;
+                }
+            }
+        }
+
+        if rsp_buf.capacity() - rsp_buf.len() <= 512 {
+            rsp_buf.reserve(4096 * 32);
+        }
+
+        // prepare the reqs
+        while let Some(req) = t!(request::decode(&mut req_buf)) {
+            let mut rsp = Response::new(&mut body_buf);
+            if let Err(e) = service.call(req, &mut rsp) {
+                rsp = internal_error_rsp(e, &mut body_buf);
+            }
+            response::encode(rsp, &mut rsp_buf);
+        }
+
+        if !rsp_buf.is_empty() {
+            let len = rsp_buf.len();
+            let mut written = 0;
+            while written < len {
+                match stream.write(&rsp_buf[written..]) {
+                    Ok(n) => {
+                        if n == 0 {
+                            return;
+                        } else {
+                            written += n;
+                        }
+                    }
+                    Err(err) => {
+                        if err.kind() == io::ErrorKind::WouldBlock {
+                            break;
+                        } else if err.kind() == io::ErrorKind::ConnectionReset
+                            || err.kind() == io::ErrorKind::UnexpectedEof
+                        {
+                            // info!("http server read req: connection closed");
+                            return;
+                        }
+                        error!("call = {:?}\nerr = {:?}", stringify!($e), err);
+                        return;
+                    }
+                }
+            }
+            if written == len {
+                unsafe { rsp_buf.set_len(0) }
+            } else if written > 0 {
+                rsp_buf.advance(written);
+            }
+        }
+
+        stream.wait_io();
+    }
+}
+
+#[cfg(not(unix))]
 fn each_connection_loop<T: HttpService>(mut stream: TcpStream, mut service: T) {
     let mut req_buf = BytesMut::with_capacity(4096 * 8);
     let mut rsp_buf = BytesMut::with_capacity(4096 * 32);
